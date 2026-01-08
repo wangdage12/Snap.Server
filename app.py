a4d95e4f90545522c7380efa9f9d2c800c4a372f
fanbook-wangdage <124357765+fanbook-wangdage@users.noreply.github.com>
2026-01-08 23:34:42 +0800

    添加sentry、新增公告筛选功能

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from app.init import create_app
 from app.config_loader import config_loader
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://d1cad1d2b442cf8431df3ee4bab925e0@o4507525750521856.ingest.us.sentry.io/4510623668830208",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    traces_sample_rate=1.0,
+)
 
 # 创建应用实例
 app = create_app()
--- services/announcement_service.py
+++ services/announcement_service.py
@@ -1,11 +1,19 @@
-from app.extensions import client
+from app.extensions import client, logger
 from app.config import Config
 
-def get_announcements():
+def get_announcements(request_data: list):
     if Config.ISTEST_MODE:
         return []
-    
+    # 记录请求体到日志，请求体中是用户已关闭的公告ID列表
+    logger.debug("Request body: %s", request_data)
+
     announcements = list(client.ht_server.announcement.find({}))
+    result = []
     for a in announcements:
+        # 拷贝并移除 _id 字段，避免 ObjectId 无法序列化
+        a = dict(a)
         a.pop('_id', None)
-    return announcements
+        # 如果请求体中包含该公告ID，说明用户已关闭该公告，不返回该公告
+        if a.get('Id') not in request_data:
+            result.append(a)
+    return result
